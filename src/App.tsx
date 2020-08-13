b5551e0bbed7250e72661d042ed8b8f6c48e07e6
Rémi Vion <vion.remi@gmail.com>
2020-08-13 12:31:36 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>BLOG</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <p>BLOG v2</p>
             </header>
         </div>
     )
